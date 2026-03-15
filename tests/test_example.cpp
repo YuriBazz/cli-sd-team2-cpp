d2b239277546f81d98919384310876d622d2408d
VillebboisLU <villeboislu@gmail.com>
2026-03-15 16:43:26 +0300

    Format changes

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,14 +1,12 @@
 #include <gtest/gtest.h>
 
-TEST(Module1Test, BasicFunctionality) {
-    EXPECT_EQ(1 + 1, 2);
-}
+TEST(Module1Test, BasicFunctionality) { EXPECT_EQ(1 + 1, 2); }
 
 /*TEST(Module1Test, AnotherTest) {
 
 }*/
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
